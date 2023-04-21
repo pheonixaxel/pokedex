2467dbe43d760f123a69e81044a9dbdd93d01c6e
Alex <alex.cimpan0@yahoo.com>
2023-04-21 11:10:41 +0200

    PokeAPI connected, retrieve all pokemons from the api and Pokemon Object created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [allPokemons, setAllPokemons] = useState([])
+  const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon?limit=20")
+
+  const getAllPokemons = async() => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    async function createPokemonObject(results) {
+      results.forEach( async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`) 
+        const data = await res.json()
+        
+        setAllPokemons(currentList => [...currentList, data]) // allPokemons.push(data)
+        
+        await console.log(allPokemons)
+        
+      })
+    }
+
+    createPokemonObject(data.results)
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getAllPokemons()
+  }, [])
+
   return (
-    <h1>Hello on my Pokedex</h1>
+    <div className="app-container">
+      <h1>Hello to my Pokedex</h1>
+      <div className="all-container">
+        <button className="load-more">Load more</button>
+      </div>
+    </div>
   );
 }
 
